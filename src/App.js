76db5755cfd3a90c899cc6fe4f5775cddb3a7703
Daniel Mattox <dmattox10@gmail.com>
2019-12-25 16:43:43 -0500

    More scaffolding, including components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+
+import Auth from './components/Auth';
+import Feed from './components/Feed';
+import Saved from './components/Saved';
+
+let auth = false;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className='App'>
+      <Route exact path='/'>
+        <Feed />
+      </Route>
+      <Route exact path='/auth'>
+        <Auth />
+      </Route>
+      <Route exact path='/saved'>
+        <Saved />
+      </Route>
+      {
+        auth ? 
+        <Link to='/saved'>View Saved Posts</Link> :
+        <Link to='/auth'>Log In or Sign Up</Link>
+      };
+      
+
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from 'reactstrap';
+
+const Auth = () => {
+    return (
+        <>
+        
+        </>
+    );
+}
+
+export default Auth;
