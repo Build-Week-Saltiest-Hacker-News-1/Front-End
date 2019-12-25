865666a149536660c90577a7df3ee81fd10789c7
Daniel Mattox <dmattox10@gmail.com>
2019-12-25 17:13:45 -0500

    Added Router, placeholder components, tabbed area for login and signup forms

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import { Route, Link } from 'react-router-dom'
 import Auth from './components/Auth';
 import Feed from './components/Feed';
 import Saved from './components/Saved';
+import { Container } from 'reactstrap';
 
 let auth = false;
 
 function App() {
   return (
     <div className='App'>
-      <Route exact path='/'>
-        <Feed />
-      </Route>
-      <Route exact path='/auth'>
-        <Auth />
-      </Route>
-      <Route exact path='/saved'>
-        <Saved />
-      </Route>
       {
-        auth ? 
-        <Link to='/saved'>View Saved Posts</Link> :
-        <Link to='/auth'>Log In or Sign Up</Link>
-      };
-      
-
+          auth ? 
+          <Link to='/saved'>View Saved Posts</Link> :
+          <Link to='/auth'>Log In or Sign Up</Link>
+        };
+      <Container>
+        <Route exact path='/'>
+          <Feed />
+        </Route>
+        <Route exact path='/auth'>
+          <Auth />
+        </Route>
+        <Route exact path='/saved'>
+          <Saved />
+        </Route>
+      </Container>
     </div>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import {} from 'reactstrap';
+import React, { useState } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
 const Auth = () => {
-    return (
-        <>
-        
-        </>
-    );
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+      if(activeTab !== tab) setActiveTab(tab);
+  }
+
+  return (
+    <div>
+      <Nav tabs>
+        <NavItem style={{width: '50%'}}>
+            <NavLink 
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}
+            >Log In</NavLink>
+        </NavItem>
+        <NavItem style={{width: '50%'}}>
+            <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggle('2'); }}
+            >Sign Up</NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col xs='6'>
+              <h4>&nbsp;Log In:</h4>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col xs='6'></Col>
+            <Col xs='6'>
+              <h4>Sign Up:</h4>
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
+    </div>
+  );
 }
 
 export default Auth;
