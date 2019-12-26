0161103ac0c3c240b74a60c1be793ca69d37e473
Daniel Mattox <dmattox10@gmail.com>
2019-12-25 20:29:38 -0500

    Added Formik and Yup without form elements

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import classnames from 'classnames';
 import axios from 'axios';
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const Auth = () => {
   const [activeTab, setActiveTab] = useState('1');
@@ -10,13 +12,80 @@ const Auth = () => {
       if(activeTab !== tab) setActiveTab(tab);
   }
   
-  const onLogIn = () => {
+  const signUp = useFormik({
+    initialValues: {
+      name: 'Your Full Name',
+      user: 'Username',
+      email: 'Your Email Address',
+      password: 'Your Password',
+      confirm: 'Retype Password',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+      .max(25, 'must be 25 characters or less')
+      .required('Required'),
+      user: Yup.string()
+      .max(20, 'must be 20 characters or less')
+      .required('Required'),
+      email: Yup.string()
+      .email('Invalid email address')
+      .required('Required'),
+      pass: Yup.string()
+      .min(8, 'must be 8 characters or more')
+      .required('Required'),
+      confirm: Yup.string().when("pass", {
+      is: val => (val && val.length > 0 ? true : false),
+      then: Yup.string().oneOf(
+          [Yup.ref("pass")],
+          "Both passwords need to be the same"
+          )
+      }),  
+      accepted: Yup.boolean()
+      .required('Required')
+      .oneOf([true], 'You must accept the terms and conditions.')
+    }),
+    onSignUp: values => {
+      axios({
+        method: 'post',
+        url: 'api/url/here',
+        data: values,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then(res => {
+        // Do Action After Sign up, do we automatically log in?
+      })
+    }
+  })
 
-  }
+  const signIn = useFormik({
+    initialValues: {
+      username: 'Username',
+      password: 'Password'
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+      .required('Required'),
+      password: Yup.string()
+      .required('Required')
+    }),
+    onSignIn: values => {
+      axios({
+        method: 'post',
+        url: 'api/url/here',
+        data: values,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then(res => {
+        // Do Login Here
+      })
+    },
+  })
 
-  const onSignUp = () => {
-
-  }
+  
 
   return (
     <div>
