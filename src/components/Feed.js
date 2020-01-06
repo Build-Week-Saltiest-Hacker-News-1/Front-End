8bc811472a2923963b6e4ea08a6e619af77299a5
squashgray <squashgray@gmail.com>
2020-01-06 14:32:10 -0800

    privateroute start

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Feed = () => {
-    return (
-        <>
-            <h1>HECK YEAH</h1>
-        </>
-    );
-}
+const Feed = props => {
+  return (
+    <>
+      <h1>HECK YEAH</h1>
+    </>
+  );
+};
 
 export default Feed;
