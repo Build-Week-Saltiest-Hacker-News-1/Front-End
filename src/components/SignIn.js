f4c769d73c4901386b7a8a6b868fb19b1e9b2a6b
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-06 15:24:26 -0500

    added provider, other redux stuff in index, and fixed the login and it works

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Card, CardTitle, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import axios from 'axios';
 import { useFormik } from 'formik';
+
+import { useDispatch} from 'react-redux'
+import { postLogin } from './../actions'
 import * as Yup from 'yup';
 
 const SignIn = () => {
+
+    const dispatch = useDispatch()
+
+    const handleSubmitLogin = e => {
+        e.preventDefault()
+        dispatch(postLogin(signIn.values))
+    }
     
     const signIn = useFormik({
         initialValues: {
@@ -17,20 +26,7 @@ const SignIn = () => {
           password: Yup.string()
           .required('Required')
         }),
-        onSignIn: values => {
-          axios({
-            method: 'post',
-            url: 'https://salty-hacker-news.herokuapp.com/api/auth/login',
-            data: values,
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-          })
-          .then(res => {
-            // Do Login Here
-            console.log(res)
-          })
-        },
+
       });
 
     return (
@@ -42,7 +38,7 @@ const SignIn = () => {
                     <Card>
                         <CardTitle>Sign In:</CardTitle>
                         <Col xs='12'>
-                            <Form onSubmit={signIn.onSignIn}>
+                            <Form onSubmit={handleSubmitLogin}>
                                 <FormGroup>
                                     <Label for='user'>Username</Label>
                                      <Col xs='12'>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import 'bootswatch/dist/slate/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { logger } from 'redux-logger'
+import { Reducers } from './reducers';
+
+
+const store = createStore(Reducers, applyMiddleware(thunk, logger))
+
+
+ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
