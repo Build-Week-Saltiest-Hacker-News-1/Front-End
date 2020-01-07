9c3080b21c9e234f52b93d739c5b9d45bc02b7b9
Daniel Mattox <dmattox10@gmail.com>
2020-01-07 13:11:33 -0500

    Can't get users to map

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Row, Col } from 'reactstrap'
+import axios from 'axios'
+
+import UserCard from "./UserCard";
 
 const UserList = () => {
+    const [usersList, updateUsers] = useState({})
+
+    useEffect(() => {
+        axios.get('https://swapi.co/api/people')
+            .then(res => {
+                console.log(res)
+                updateUsers(res.data.results)
+            })
+    }, [])
+
     return (
         <>
-            <h1>Testing</h1>
+            <Row>
+                <Col xs="1"></Col>
+                <Col xs="3"><h4 className="text-primary">User</h4></Col>
+                <Col xs="1"></Col>
+                <Col xs="4"><h4 className="text-primary">Comment</h4></Col>
+                <Col xs="1"></Col>
+                <Col xs="1"><h4 className="text-primary">Score</h4></Col>
+                <Col xs="1"><h4 className="text-primary">Save</h4></Col>
+            </Row>
+            {usersList.map(user => 
+                    <UserCard user={ user } />    
+                )
+            }
         </>
     );
 }
