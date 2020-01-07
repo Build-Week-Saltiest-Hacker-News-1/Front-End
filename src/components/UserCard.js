eb86e1fcba45162d6d87be0b4e73cccc193e62ac
Daniel Mattox <dmattox10@gmail.com>
2020-01-07 13:31:11 -0500

    Added UserList and UserCard components with req

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Row, Col, Table } from "reactstrap";
 const UserCard = (props) => {
+    const { user } = props;
     return(
-        <>
-
-        </>
-    )
+        <div>
+            <div className="bg-secondary">
+                <Row>    
+                    <Col xs="1"></Col>
+                    <Col xs="3"><h4 className="text-primary">{user.name}</h4></Col>
+                    <Col xs="1"></Col>
+                    <Col xs="4"><h4 className="text-primary">{user.homeworld}</h4></Col>
+                    <Col xs="1"></Col>
+                    <Col xs="1"><h4 className="text-primary">{user.height}</h4></Col>
+                    <Col xs="1"><h4 className="text-primary">Save</h4></Col>
+                </Row>
+            </div>
+            <Row>&nbsp;</Row>
+        </div>
+    );
 }
 
-export default UserCard
+export default UserCard;
