1797096aa8bdd1f3ed604bbc52cbc60aeaaf8a8e
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-06 20:22:38 -0500

    login works

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
@@ -8,13 +9,15 @@ export const REGISTER_START = "REGISTER_START";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 
-export const postLogin = (payload, history) => dispatch => {
+export const postLogin = (payload) => dispatch => {
   dispatch({ type: LOGIN_START });
-  axios
-    .post("https://salty-hacker-news.herokuapp.com/api/auth/login", payload)
+  axiosWithAuth()
+    .post("api/auth/login/", payload.values)
     .then(res => {
       dispatch({ type: LOGIN_SUCCESS, payload: res.data });
-      history.push("/feed");
+      console.log(res.data.token)
+      localStorage.setItem("token", res.data.token)
+      payload.props.history.push("/feed");
     })
     .catch(err => {
       dispatch({ type: LOGIN_FAIL, payload: err.response });
@@ -23,8 +26,8 @@ export const postLogin = (payload, history) => dispatch => {
 
 export const postRegister = payload => dispatch => {
   dispatch({ type: REGISTER_START });
-  axios
-    .post("https://salty-hacker-news.herokuapp.com/api/auth/register/", payload)
+  axiosWithAuth()
+    .post("api/auth/register/", payload)
     .then(res => {
       dispatch({ type: REGISTER_SUCCESS, payload: res.data });
     })
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "",
+    baseURL: "https://salty-hacker-news.herokuapp.com/",
     headers: {
       Authorization: token
     }
